8947edacd7cae13c8dfd1ed9137038452fd5713f
Malhesidek <malhesidek@gmail.com>
2024-04-05 10:03:27 +0300

    app can add new tasks and checks if inputted value is not empty

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_draft/business_logic/task_list_bloc/bloc/task_list_bloc.dart';
 import 'package:todo_draft/constants.dart';
 import 'package:todo_draft/data/models/task.dart';
 import 'package:todo_draft/presentation/widgets/task_field_widget.dart';
@@ -38,18 +40,27 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: kTitleApp,
-        centerTitle: true,
-        backgroundColor: kColorBlue1,
-      ),
-      body: Container(
-        child: Column(
-          children: [
-            TaskFieldWidget(),
-            TaskWidget(task: Task(name: "First task", isActive: true),),
-          ],
+    final taskListBloc = TaskListBloc();
+    return BlocProvider(
+      create: (context) => taskListBloc,
+      child: Scaffold(
+        appBar: AppBar(
+          title: kTitleApp,
+          centerTitle: true,
+          backgroundColor: kColorBlue1,
+        ),
+        body: Container(
+          child: BlocBuilder<TaskListBloc, TaskListState>(
+            builder: (context, state) {
+              return Column(
+                      children: [
+                        TaskFieldWidget(),
+                        if (state is TaskListEmptyState) Text("No tasks for now")
+                        else if (state is TaskListChangedState) for (var task in state.taskList) TaskWidget(task: task)
+                      ],
+                    );
+            },
+          ),
         ),
       ),
     );
--- lib/presentation/widgets/task_field_widget.dart
+++ lib/presentation/widgets/task_field_widget.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_draft/business_logic/task_list_bloc/bloc/task_list_bloc.dart';
 import 'package:todo_draft/constants.dart';
+import 'package:todo_draft/data/models/task.dart';
 
 class TaskFieldWidget extends StatefulWidget {
   const TaskFieldWidget({super.key});
@@ -11,8 +14,10 @@ class TaskFieldWidget extends StatefulWidget {
 }
 
 class _TaskFieldWidgetState extends State<TaskFieldWidget> {
+  String? taskName;
   @override
   Widget build(BuildContext context) {
+    final taskListBloc = BlocProvider.of<TaskListBloc>(context);
     return Container(
       decoration: BoxDecoration(
           borderRadius: BorderRadius.all(Radius.circular(4)),
@@ -26,11 +31,17 @@ class _TaskFieldWidgetState extends State<TaskFieldWidget> {
                 border: OutlineInputBorder(),
                 hintText: "Enter your task",
                 contentPadding: EdgeInsets.all(8)),
+            onChanged: (text) {
+              setState(() {
+                taskName = text;
+              });
+            },
           )),
           IconButton(
-              onPressed: () {
-                //TODO add function from taskList BLOC
-              },
+              onPressed: taskName != null && taskName!.isNotEmpty ? () {
+                taskListBloc.add(TaskListAddedTaskEvent(
+                    task: Task(name: taskName!, isActive: false)));
+              } : null,
               icon: Icon(Icons.send))
         ],
       ),
