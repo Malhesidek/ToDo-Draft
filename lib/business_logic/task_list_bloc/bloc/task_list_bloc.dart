cd9205075dc72409f007b44d6457f025b89ccb4e
Malhesidek <malhesidek@gmail.com>
2024-04-05 09:22:04 +0300

    second variant of bloc addTask

--- lib/business_logic/task_list_bloc/bloc/task_list_bloc.dart
+++ lib/business_logic/task_list_bloc/bloc/task_list_bloc.dart
@@ -8,17 +8,17 @@ part 'task_list_state.dart';
 class TaskListBloc extends Bloc<TaskListEvent, TaskListState> {
   TaskListBloc() : super(TaskListEmptyState()) {
     on<TaskListAddedTaskEvent>((event, emit) {
-      var copiedTaskList;
-      if (state is TaskListEmptyState) {
-        TaskListEmptyState emptyState = state as TaskListEmptyState;
-        List<Task> copiedTaskList = List<Task>.from(emptyState.taskList);
-        // Використовуйте скопійовані дані copiedTaskList
-      } else if (state is TaskListChangedState) {
-        TaskListChangedState changedState = state as TaskListChangedState;
-        List<Task> copiedTaskList = List<Task>.from(changedState.taskList);
-        // Використовуйте скопійовані дані copiedTaskList
+      // Отримуємо поточний стан
+      final currentState = state;
+
+      if (currentState is TaskListChangedState) {
+        // Створюємо новий стан зі списком завдань, який включає нове завдання
+        final List<Task> updatedTaskList = List.from(currentState.taskList)..insert(0, event.task);
+        emit(TaskListChangedState(taskList: updatedTaskList));
+      } else {
+        // Якщо поточний стан не є TaskListChangedState, просто додаємо нове завдання до порожнього списку
+        emit(TaskListChangedState(taskList: [event.task]));
       }
-      emit(TaskListChangedState(taskList: copiedTaskList));
     });
   }
 }
